276cb04118eb183b6e76679417bac69ca045b420
Matteo Ranzi <matteoranzi2000@gmail.com>
2025-12-31 01:16:10 +0100

    Add ConsoleSink header file; define header guard and include TODO for mutex usage with ConsoleSource.

---
+++ include/PipeX/nodes/thread_safe/ConsoleSink.h
@@ -0,0 +1,10 @@
+//
+// Created by Matteo Ranzi on 31/12/25.
+//
+
+#ifndef PIPEX_CONSOLESINK_H
+#define PIPEX_CONSOLESINK_H
+
+//TODO use the same mutex for both ConsoleSink and ConsoleSource
+
+#endif //PIPEX_CONSOLESINK_H
